ac8fce42915b83382fd0c4803ba282390e59a351
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-10-14 23:27:49 -0300

    test: ensure AccountMongoRepository update the account accessToken on updateAccessToken sucess

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -58,4 +58,19 @@ describe('Account Mongo Repository', () => {
     const account = await sut.loadByEmail('any_email@mail.com')
     expect(account).toBeFalsy()
   })
+
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut()
+    await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    const accountWithoutAccessToken = await accountCollection.findOne({ email: 'any_email@mail.com' })
+    expect(accountWithoutAccessToken.accessToken).toBeFalsy()
+    await sut.updateAccessToken(accountWithoutAccessToken._id.toString(), 'any_token')
+    const accountWithAccessToken = await accountCollection.findOne({ _id: accountWithoutAccessToken._id })
+    expect(accountWithAccessToken).toBeTruthy()
+    expect(accountWithAccessToken.accessToken).toBe('any_token')
+  })
 })
