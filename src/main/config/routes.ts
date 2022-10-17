0c98aff6774579ad0044819b814aa33037f9dce3
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-10-17 20:29:23 -0300

    fix: exclude sourceMap files from running on production

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,12 +1,12 @@
 import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
-import path from 'path'
+import { join } from 'path'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  readdirSync(path.join(__dirname, '/../routes')).map(async file => {
-    if (!file.includes('.test.')) {
+  readdirSync(join(__dirname, '../routes')).map(async file => {
+    if (!file.includes('.map') && !file.includes('.test.')) {
       (await import(`../routes/${file}`)).default(router)
     }
   })
