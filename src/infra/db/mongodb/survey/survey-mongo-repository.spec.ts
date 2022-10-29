ae7c353b3b926a2074e742066407a4cb9c9059f0
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-10-28 21:54:19 -0300

    test: ensure SurveyMongoRepository find survey by id on success

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -73,4 +73,21 @@ describe('Survey Mongo Repository', () => {
       expect(surveys.length).toBe(0)
     })
   })
+
+  describe('loadById()', () => {
+    test('Should load survey by id on success', async () => {
+      const res = await surveyCollection.insertOne({
+        question: 'any_question',
+        answers: [{
+          image: 'any_image',
+          answer: 'any_answer'
+        }],
+        date: new Date()
+      })
+      const surveyFromCollection = await surveyCollection.findOne({ _id: res.insertedId })
+      const sut = makeSut()
+      const survey = await sut.loadById(surveyFromCollection._id.toString())
+      expect(survey).toBeTruthy()
+    })
+  })
 })
