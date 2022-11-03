83e6e3188a83fa8d1b701f583fe845d35304c024
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-11-03 12:08:33 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,29 @@
+import { loginParamsSchema } from './schemas/login-params-schema'
+import { accountSchema } from './schemas/account-schema'
+import { loginPath } from './paths/login-path'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'Survey API',
     version: '1.0.0'
+  },
+  servers: [
+    {
+      url: '/api'
+    }
+  ],
+  tags: [
+    {
+      name: 'Login'
+    }
+  ],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
